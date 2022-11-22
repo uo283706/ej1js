009c14e2e566c480f41e13c343ae1fb27d1396ee
uo283706 <113984425+uo283706@users.noreply.github.com>
2022-11-23 00:31:56 +0100

    Add files via upload

---
+++ Cabecera.js
@@ -0,0 +1,8 @@
+var asignatura = new Object();
+asignatura.nombre = "Software y estándares en la Web";
+asignatura.titulacion = "Grado en Ingeniería Informática del Software";
+asignatura.centro = "Escuela de Ingeniería Informática";
+asignatura.universidad = "Universidad de Oviedo";
+asignatura.curso = "2022-2023";
+asignatura.alumno = "Sara Lamuño García";
+asignatura.email = "uo283706@uniovi.es";
---
+++ Parrafos.js
@@ -0,0 +1,9 @@
+document.write("<p>");
+document.write(asignatura.curso);
+document.write("</p>");
+document.write("<p>");
+document.write(asignatura.alumno);
+document.write("</p>");
+document.write("<p>");
+document.write(asignatura.email);
+document.write("</p>");
